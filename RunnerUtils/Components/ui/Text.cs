fde39e3e843287d9a2453d4f9bc8611cfa1d4f9d
Mitchell Merry <mitchellmerry145@gmail.com>
2026-04-07 20:40:40 +1000

    Implement custom settings

---
+++ RunnerUtils/Components/ui/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fleece;
+
+namespace RunnerUtils.Components.UI
+{
+    internal class Text
+    {
+        private static int currentOffset = 0;
+
+        // not sure exactly what is needed here - this seems to work
+        // see https://kaiclavier.com/docs/Fleece.html#what-is-a-parser
+        public static Jumper MakeJumper(string text)
+        {
+            Passage customPassage = new();
+            // trying to protect from collisions. if I'm honest with you I don't even know what would happen if we did collide
+            customPassage.id = Story.active.passages.Count + 100000 + currentOffset++;
+            customPassage.text = text;
+            Story.active.passages.Add(customPassage);
+
+            Jumper j = new();
+            j.passage = customPassage;
+            return j;
+        }
+    }
+}
